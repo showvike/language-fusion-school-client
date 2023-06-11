1ffb681a9a6b8dbdd71fcb86bba20d166c3b2667
showvike <showvike@gmail.com>
2023-06-12 00:19:07 +0600

    classes done

--- src/pages/home/Classes/Classes.jsx
+++ src/pages/home/Classes/Classes.jsx
@@ -1,7 +1,25 @@
+import { useEffect, useState } from "react";
+import Card from "../../shared/Card/Card";
+
 const Classes = () => {
+  const [classes, setClasses] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://b7a12-summer-camp-server-side-showvike.vercel.app/classes?type=popular&limit=6"
+    )
+      .then((res) => res.json())
+      .then((data) => setClasses(data));
+  }, []);
+
   return (
     <div className="h-screen bg-classes-texture">
       <h1 className="font-bold text-5xl text-center pt-4">Popular Classes</h1>
+      <div className="pt-8 grid grid-cols-3 px-8 justify-items-center gap-8">
+        {classes.map((course) => (
+          <Card key={course._id} item={course} />
+        ))}
+      </div>
     </div>
   );
 };
---
+++ src/pages/shared/Card/Card.jsx
@@ -0,0 +1,23 @@
+const Card = ({ item }) => {
+  const { image, name } = item;
+  const words = name.split(" ");
+
+  return (
+    <div className="card w-96 bg-base-100 shadow-xl image-full">
+      <figure>
+        <img src={image} />
+      </figure>
+      <div className="card-body flex flex-col justify-center gap-4 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+        <div>
+          <h2 className="card-title text-3xl">{words[0]}</h2>
+          <h2 className="card-title">
+            <small>{words[1]}</small>
+          </h2>
+        </div>
+        <h2 className="card-title self-end text-3xl">{words[2]}</h2>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
