2f2a48e8d627b8a19ea72c13868a326987a81e88
showvike <showvike@gmail.com>
2023-06-14 20:46:11 +0600

    404 done

--- src/pages/shared/Error/Error.jsx
+++ src/pages/shared/Error/Error.jsx
@@ -1,10 +1,13 @@
-import { useRouteError } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 
 const Error = () => {
   const error = useRouteError();
   console.log(error);
   return (
     <div className="bg-error-texture h-screen bg-no-repeat bg-cover flex flex-col justify-center items-center space-y-16">
+      <Link to="/">
+        <button className="btn btn-primary">Back To Home</button>
+      </Link>
       <h1 className="font-bold text-9xl">Oops!</h1>
       <p className="text-xl">Sorry, an unexpected error has occurred.</p>
       <p className="font-medium text-5xl">
