0a97e68b83b4c329636caee26864f7d09c24c7c4
showvike <showvike@gmail.com>
2024-03-29 16:42:58 +0600

    Update App.jsx
    for test

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,10 @@ function App() {
       "https://b7a12-summer-camp-server-side-showvike-showvike.vercel.app/db"
     )
       .then((res) => res.json())
-      .then((data) => setMessage(data.message));
+      .then((data) => {
+        console.log(data);
+        setMessage(data.message);
+      });
   }, []);
 
   return <>{<button className="btn btn-lg btn-wide">{message}</button>}</>;
